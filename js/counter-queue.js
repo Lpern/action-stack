3c6cc48def78a7a452510bc340ace33d5f35144f
Lepren <seekpreston@gmail.com>
2021-01-13 10:19:10 -0800

    comments and cleanup
    If you want to use this you need to add text to your redeem events because twitch WS stuffs says no to redeeme events that don't include text (you'd need webhooks).

--- js/counter-queue.js
+++ js/counter-queue.js
@@ -1,3 +1,4 @@
+// not the cleanest implementation of this idea, but should be easier to read than the slim version.
 class counterQueue{
 
     queue = Array();
@@ -8,7 +9,7 @@ class counterQueue{
     
     constructor(){
 
-        /*$.getJSON("settings.json",
+        /*$.getJSON("settings.json", // If you're hosting this feel free to include the settings.json instead of the below settings
             function (data) {
                 this.options = data;
                 console.log(JSON.stringify(this.options));
@@ -31,7 +32,7 @@ class counterQueue{
 
     startQueueTick()
     {
-        /*
+        /* some more test cases if you need them in the starting function
         this.queue = [
             {"claiment":"tom", "action":"deactivate"},
             {"claiment":"bob", "action":"activate"},
@@ -40,73 +41,72 @@ class counterQueue{
             {"claiment":"josh", "action":"deactivate"},
         ];
         */
-        this.interval = setInterval(this.queueTick.bind(this),(this.options.waitTillAction * 1000));
+        this.interval = setInterval(this.queueTick.bind(this),(this.options.waitTillAction * 1000)); // sets the queue to tick
     }
 
     queueTick()
     {
-        //console.log("----------------------------");
-        //console.log("STATUS: "+this.current);
-        if(Array.isArray(this.queue) && this.queue.length >= 1)
+        //console.log("----------------------------"); // debug
+        //console.log("STATUS: "+this.current); // debug
+        if(Array.isArray(this.queue) && this.queue.length >= 1) // there is more queue to execute?
         {
-            var top = this.queue.pop();
-            //console.log("claiment: "+top.claiment+" action: "+top.action);
-            if(top.action != this.options.reversalAction)
+            var top = this.queue.pop(); // take the last element out of the queue to act on it.
+            //console.log("claiment: "+top.claiment+" action: "+top.action); // debug
+            if(top.action != this.options.reversalAction) // is it NOT a reversal action?
             {
-                if(this.reverse < 1 || (this.reverse >= 2 && this.reverse % 2 == 0))
+                if(this.reverse < 1 || (this.reverse >= 2 && this.reverse % 2 == 0)) // there's an even number of stacked reverses
                 {
+                    // do what the action says
                     if(top.action == this.options.positiveAction)
                         this.current = this.options.positiveTitle;
                     if(top.action == this.options.negativeAction)
                         this.current = this.options.negativeTitle;
                     this.reverse = 0;
                 }
-                else if(this.reverse >= 1 && this.reverse % 2 == 1)
+                else if(this.reverse >= 1 && this.reverse % 2 == 1) // there's an odd number of stacked reverses
                 {
+                    // do the opposite of what the action says
                     if(top.action == this.options.positiveAction)
                         this.current = this.options.negativeTitle;
                     if(top.action == this.options.negativeAction)
                         this.current = this.options.positiveTitle;
 
-                    $("#reverse-card").show(1000);
+                    $("#reverse-card").show(1000); // show the reverse card being used
                     $("#reverse-card").hide(50);
                     this.reverse = 0;
                 }
-                //console.log("current: "+this.current+" flip: "+this.reverse);
-                this.updateGUI();
+                //console.log("current: "+this.current+" flip: "+this.reverse); // debug
+                this.updateGUI(); // update the GUIstack
                 
-                //setTimeout(this.queueTick(),10000); 
                 return;
             }
             if(top.action == this.options.reversalAction)
             {
-                this.reverse++;
-                $("#reverse-card").show();
+                this.reverse++; // we've added a reverse to the reverse stack
+                $("#reverse-card").show(); // show us putting the reverse card down as a trap
                 $("#reverse-card").hide(3000);
 
-                this.updateGUI();
-                //console.log("current: "+this.current+" flip: "+this.reverse);
-                //setTimeout(this.queueTick(),3000); 
+                this.updateGUI(); // update the GUIstack
+                //console.log("current: "+this.current+" flip: "+this.reverse); // debug
                 return;
             }
             
         }
-        //setTimeout(this.queueTick(),10000);
         return;
     }
 
     updateGUI()
     {
-        $("#vtguistack").empty();
-        if(this.queue.length > 0)
+        $("#vtguistack").empty(); // clear the existing stack
+        if(this.queue.length > 0) // make sure the queue isn't empty
         {
             var i;
-            for(i = this.queue.length - 1; i > 0 ; i--)
+            for(i = this.queue.length - 1; i > 0 ; i--) // reverse for iteration loop since the front needs to be the top of the GUI stack
             {
-                var clmnt = $("<td></td>").text(this.queue[i].claiment);
-                var actn = $("<td></td>").text(this.queue[i].action);
+                var clmnt = $("<td></td>").text(this.queue[i].claiment); // add css to the table row details here if you need
+                var actn = $("<td></td>").text(this.queue[i].action); // ""
         
-                var row = $("<tr></tr>").append(clmnt,actn);
+                var row = $("<tr></tr>").append(clmnt,actn); // ""
 
                 $("#vtguistack").append(row);
             }
@@ -118,18 +118,18 @@ class counterQueue{
     {
         if(action == this.options.reversalAction)
         {
-            this.queue.push({"claiment":claiment, "action":this.options.reversalAction});
+            this.queue.push({"claiment":claiment, "action":this.options.reversalAction}); // push instead of unshift for reverse because we want that to go first every time
         }
         else if(action == this.options.positiveAction)
         {
-            this.queue.unshift({"claiment":claiment, "action":this.options.positiveAction});
+            this.queue.unshift({"claiment":claiment, "action":this.options.positiveAction}); // unshift the new action to the back
         }
         else if(action == this.options.negativeAction)
         {
-            this.queue.unshift({"claiment":claiment, "action":this.options.negativeAction});
+            this.queue.unshift({"claiment":claiment, "action":this.options.negativeAction}); // unshift the new action to the back
         }
         
-        this.updateGUI();
+        this.updateGUI(); // this is kind-of optional since the GUI will refresh every 10s anyways
         return;
     }
 }
